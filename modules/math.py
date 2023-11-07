e3f9bd0c06efe4aaa6a3790cff58e85466102c09
jssatorres <juansataz.dev@gmail.com>
2023-11-07 07:49:39 +0100

    feat: example math module

---
+++ modules/math.py
@@ -0,0 +1,44 @@
+import sys, math
+# import sys as s
+from sys import exit as bye_bye
+
+# for name in dir(math):
+#   print(name)
+  
+
+
+print(math.ceil(3.1))
+print(math.ceil(3.505))
+print(math.ceil(3.945))
+print(math.ceil(3.2))
+print(math.ceil(-5.4))
+
+
+print(math.floor(3.1))
+print(math.floor(3.505))
+print(math.floor(3.945))
+print(math.floor(3.2))
+print(math.floor(-5.4))
+
+print(math.trunc(3.1))
+print(math.trunc(3.505))
+print(math.trunc(3.945))
+print(math.trunc(3.2))
+print(math.trunc(-5.4))
+
+print(math.factorial(3)) # 3-2-1
+print(math.factorial(4)) # 4*3*2*1
+
+print(math.sqrt(25)) #5
+print(math.sqrt(100)) # 4
+
+print(math.hipot(3,4)) # damos los catetors y calcula la hipotenusa
+print(math.hipot(4,5)) 
+
+# def exitFunc():
+#   print(' i wanna to exitFunc')
+  
+# exitFunc()
+# sys.exit()
+# s.exit()
+bye_bye()
