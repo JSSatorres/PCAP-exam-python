34741273fa610316aa44fe9940d25cdd8fd65245
jssatorres <juansataz.dev@gmail.com>
2023-11-07 07:50:20 +0100

    feat: example sys module

---
+++ modules/sys.py
@@ -0,0 +1,19 @@
+# import sys, math
+# import sys as s
+# from sys import exit as bye_bye
+
+
+from sys import exit
+
+# for name in dir(sys):
+#   print(name)
+  
+
+def exitFunc():
+  print(' i wanna to exitFunc')
+  
+exitFunc()
+# sys.exit()
+# s.exit()
+#  bye_bye()
+exit()
